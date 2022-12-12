a5432ee0278a570a0d78c1c02ed8d162a3b930cc
Brandon Laursen <83096378+brandonlaursen@users.noreply.github.com>
2022-12-12 18:32:33 -0500

    Update question-2.js

--- comprehension-questions/question-2.js
+++ comprehension-questions/question-2.js
@@ -7,12 +7,26 @@ Comprehension Question #2
 Draw out the execution of this code. Explain how the event loop processes each of the function calls. What order will the code run and why? Explain when and why items enter and leave the call stack and message queue.
 */
 
-setTimeout(() => {
-  console.log('first');
-}, 5000);
 
-setTimeout(() => {
-  console.log('second');
-}, 0);
+function somethingSlow(n) {
+  if (n === 1 || n === 2) return 1;
 
-console.log('third')
+  return somethingSlow(n - 1) + somethingSlow(n - 2);
+}
+
+function foo() {
+  console.log("food");
+}
+
+function bar() {
+  console.log("bark");
+  baz();
+}
+
+function baz() {
+  console.log("bazaar");
+}
+
+setTimeout(foo, 1500);
+setTimeout(bar, 1000);
+console.log(somethingSlow(44));
